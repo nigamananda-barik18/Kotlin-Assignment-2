10cdefea2475161bba2643bb4900a9153da75130
nigamananda-barik18 <bariknigam418@gmail.com>
2026-05-30 14:41:36 +0530

    Create q9.kt

---
+++ q9.kt
@@ -0,0 +1,17 @@
+class IDGenerator {
+
+    companion object {
+        private var currentId = 0
+
+        fun getNextId(): Int {
+            currentId++
+            return currentId
+        }
+    }
+}
+
+fun main() {
+    println(IDGenerator.getNextId())
+    println(IDGenerator.getNextId())
+    println(IDGenerator.getNextId())
+}
